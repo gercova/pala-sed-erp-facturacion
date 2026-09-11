e865655ff5dd0b0a4f5c8c0bd9090d2c160376de
gercova <germancotrina17@gmail.com>
2026-09-11 08:24:21 -0500

    feat: rutas sin verificación de token CSRF incluidas

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'pedido/store',
+        'pedido/check-client',
     ];
 }
