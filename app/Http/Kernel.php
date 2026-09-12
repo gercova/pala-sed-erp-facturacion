8edee532ca316a6e23c86121ceec28fe76479b90
gercova <germancotrina17@gmail.com>
2026-09-12 01:46:34 -0500

    feat: nuevo middleware del cliente añadido

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cliente'  => \App\Http\Middleware\EnsureClienteRole::class,
     ];
 }
